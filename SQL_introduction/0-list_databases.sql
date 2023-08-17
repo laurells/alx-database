b954acf8bcaad3a365c6b4d8b6d2db0bf697ec37
Laurels <laurels.echichinwo@stu.cu.edu.ng>
2023-08-17 16:24:32 +0100

    update

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,20 +1,2 @@
-/*  a script that lists all databases of your MySQL server */
-import mysql.connector host = "your_host" user = "your_user" password = "your_password" try:
-/* Connect to the MySQL server */
-connection = mysql.connector.connect(
-    host = host,
-    user = user,
-    password = password
-)
-/* Create a cursor object to interact with the server */
-cursor = connection.cursor()
 /* Execute SQL command to list all databases */
-cursor.execute("SHOW DATABASES")
-/* Fetch all the database names */
-databases = cursor.fetchall()
-/* Print the list of databases */
-print("List of databases:") for db in databases: print(db [0])
-except
-    mysql.connector.Error as err: print("Error:", err) finally:
-    /* Close the cursor and connection */
-    if cursor: cursor.close() if connection: connection.close()
+SHOW DATABASES;
