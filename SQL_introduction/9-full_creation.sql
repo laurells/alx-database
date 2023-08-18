2b66b5679e569e8047af2247ef18115d122800aa
Laurels <laurels.echichinwo@stu.cu.edu.ng>
2023-08-18 13:28:49 +0100

    update

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,14 +1,18 @@
+-- Create the database 'hbtn_0c_0' if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+
+-- Use the 'hbtn_0c_0' database
+USE hbtn_0c_0;
+
 -- Create the table 'second_table' if it doesn't already exist
-CREATE TABLE IF NOT EXISTS hbtn_0c_0 (
+CREATE TABLE IF NOT EXISTS second_table (
     id INT,
     name VARCHAR(256),
     score INT
 );
 
 -- Insert multiple rows into 'second_table'
-INSERT INTO
-    hbtn_0c_0.second_table (id, name, score)
-VALUES
+INSERT INTO second_table (id, name, score) VALUES
     (1, 'John', 10),
     (2, 'Alex', 3),
     (3, 'Bob', 14),
